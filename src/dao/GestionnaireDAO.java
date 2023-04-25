6b4e9094276a7a035d8c0fa6b7b986c7c2e4a2dc
thomassamoth <thomassamoth.dev@gmail.com>
2023-04-25 23:55:21 +0200

    Update .gitignore

--- src/dao/GestionnaireDAO.java
+++ src/dao/GestionnaireDAO.java
@@ -4,10 +4,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
-//import model.Absence;
+import model.Groupe;
 
-public class GestionnaireDAO extends ConnexionBDD{
+public class GestionnaireDAO extends ConnexionBDD {
 
 	/**
 	 * Constructeur
@@ -18,6 +19,7 @@ public class GestionnaireDAO extends ConnexionBDD{
 
 	/**
 	 * Ajoute Utilisateur dans BDD
+	 * 
 	 * @param nom
 	 * @param prenom
 	 * @param identifiant
@@ -33,8 +35,8 @@ public class GestionnaireDAO extends ConnexionBDD{
 		try {
 			con = DriverManager.getConnection(URL, LOGIN, PASSWORD);
 
-			ps = con.prepareStatement("INSERT INTO Utilisateur(nom, prenom, identifiant, motdepasse) "
-					+ "VALUES (?, ?, ?, ?)");
+			ps = con.prepareStatement(
+					"INSERT INTO Utilisateur(nom, prenom, identifiant, motdepasse) " + "VALUES (?, ?, ?, ?)");
 
 			ps.setString(1, nom);
 			ps.setString(2, prenom);
@@ -44,26 +46,28 @@ public class GestionnaireDAO extends ConnexionBDD{
 			ajoutEffectue = ps.executeUpdate();
 		} catch (Exception ee) {
 			ee.printStackTrace();
-		}
-		finally {
+		} finally {
 			// Fermeture ps
 			try {
 				if (ps != null) {
 					ps.close();
 				}
-			}catch (Exception ignore) {}
+			} catch (Exception ignore) {
+			}
 
 			// Fermeture con
 			try {
-				if(con != null)
+				if (con != null)
 					con.close();
-			}catch (Exception ignore) {}
+			} catch (Exception ignore) {
+			}
 		}
 		return ajoutEffectue;
 	}
 
 	/**
 	 * Récupère l'ID d'un utilisateur
+	 * 
 	 * @param identifiant
 	 * @param prenom
 	 * @param nom
@@ -79,7 +83,8 @@ public class GestionnaireDAO extends ConnexionBDD{
 		try {
 			con = DriverManager.getConnection(URL, LOGIN, PASSWORD);
 
-			ps = con.prepareStatement("SELECT idutilisateur FROM Utilisateur WHERE identifiant = ? AND prenom = ? AND nom = ?");
+			ps = con.prepareStatement(
+					"SELECT idutilisateur FROM Utilisateur WHERE identifiant = ? AND prenom = ? AND nom = ?");
 
 			ps.setString(1, identifiant);
 			ps.setString(2, prenom);
@@ -88,38 +93,40 @@ public class GestionnaireDAO extends ConnexionBDD{
 			// Execute la requete
 			rs = ps.executeQuery();
 
-			if(rs.next()) {
+			if (rs.next()) {
 				idUtilisateur = rs.getInt("idutilisateur");
-			}
-			else
+			} else
 				System.out.println("ID non trouvé");
 		} catch (Exception ee) {
 			ee.printStackTrace();
-		}
-		finally {
+		} finally {
 			// Fermeture ps
 			try {
 				if (ps != null) {
 					ps.close();
 				}
-			}catch (Exception ignore) {}
+			} catch (Exception ignore) {
+			}
 
 			// Fermeture con
 			try {
-				if(con != null)
+				if (con != null)
 					con.close();
-			}catch (Exception ignore) {}
+			} catch (Exception ignore) {
+			}
 		}
 		return idUtilisateur;
 	}
 
 	/**
 	 * Ajoute un etudiant dans la table
+	 * 
 	 * @param idutilisateur l'ID de l'utilisateur
-	 * @param adresseMail l'adresse mail de l'étudiant
-	 * @param groupe le groupe de l'etudiant
-	 * @param filiere la filiere de l'étudiant. Si filiere = 0 -> Apprentissage. Si filiere = 1 -> Classique 
-	 * @return ajoutEffectue 
+	 * @param adresseMail   l'adresse mail de l'étudiant
+	 * @param groupe        le groupe de l'etudiant
+	 * @param filiere       la filiere de l'étudiant. Si filiere = 0 ->
+	 *                      Apprentissage. Si filiere = 1 -> Classique
+	 * @return ajoutEffectue
 	 */
 	public int ajouterEtudiant(int idutilisateur, String adresseMail, int groupe, int filiere) {
 		Connection con = null;
@@ -130,8 +137,8 @@ public class GestionnaireDAO extends ConnexionBDD{
 		try {
 			con = DriverManager.getConnection(URL, LOGIN, PASSWORD);
 
-			ps = con.prepareStatement("INSERT INTO Etudiant(idutilisateur, mailetudiant, groupe, filiere) "
-					+ "VALUES (?, ?, ?, ?)");
+			ps = con.prepareStatement(
+					"INSERT INTO Etudiant(idutilisateur, mailetudiant, idgroupe, filiere) " + "VALUES (?, ?, ?, ?)");
 
 			ps.setInt(1, idutilisateur);
 			ps.setString(2, adresseMail);
@@ -141,25 +148,33 @@ public class GestionnaireDAO extends ConnexionBDD{
 			ajoutEffectue = ps.executeUpdate();
 		} catch (Exception ee) {
 			ee.printStackTrace();
-		}
-		finally {
+		} finally {
 			// Fermeture ps
 			try {
 				if (ps != null) {
 					ps.close();
 				}
-			}catch (Exception ignore) {}
+			} catch (Exception ignore) {
+			}
 
 			// Fermeture con
 			try {
-				if(con != null)
+				if (con != null)
 					con.close();
-			}catch (Exception ignore) {}
+			} catch (Exception ignore) {
+			}
 		}
 		return ajoutEffectue;
 	}
 
-	public int ajouterGroupe(int numeroGroupe, int capaciteMax){
+	/**
+	 * Créer un nouveau groupe
+	 * 
+	 * @param numeroGroupe le numéro du groupe
+	 * @param capaciteMax  la capacité maximale du groupe
+	 * @return 1 si ajout effectué, 0 sinon
+	 */
+	public int ajouterGroupe(int numeroGroupe, int capaciteMax) {
 		Connection con = null;
 		PreparedStatement ps = null;
 
@@ -168,8 +183,7 @@ public class GestionnaireDAO extends ConnexionBDD{
 		try {
 			con = DriverManager.getConnection(URL, LOGIN, PASSWORD);
 
-			ps = con.prepareStatement("INSERT INTO Groupe(idgroupe, numerogroupe, capacitemax)"
-					+ "VALUES (?, ?, ?)");
+			ps = con.prepareStatement("INSERT INTO Groupe(idgroupe, numerogroupe, capacitemax)" + "VALUES (?, ?, ?)");
 
 			ps.setInt(1, numeroGroupe);
 			ps.setInt(2, numeroGroupe);
@@ -178,13 +192,60 @@ public class GestionnaireDAO extends ConnexionBDD{
 			ajoutEffectue = ps.executeUpdate();
 		} catch (Exception ee) {
 			ee.printStackTrace();
-		}
-		finally {
+		} finally {
 			// Fermeture ps
 			try {
 				if (ps != null) {
 					ps.close();
 				}
+			} catch (Exception ignore) {
+			}
+
+			// Fermeture con
+			try {
+				if (con != null)
+					con.close();
+			} catch (Exception ignore) {
+			}
+		}
+		return ajoutEffectue;
+	}
+
+	public ArrayList<String> getListeGroupes(){
+		Connection con =  null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		ArrayList<String> listeGroupes = new ArrayList<>();
+
+		// Connexion à la base de données
+		try {
+			con = DriverManager.getConnection(URL, LOGIN, PASSWORD);
+
+			ps = con.prepareStatement("SELECT numeroGroupe from groupe ORDER BY numeroGroupe");
+
+			rs = ps.executeQuery();
+
+			while(rs.next()) {
+				int groupe = rs.getInt("NUMEROGROUPE");
+				listeGroupes.add(Integer.toString(groupe));
+			}
+		}
+		catch (Exception ee) {
+			ee.printStackTrace();
+		}
+
+		finally {
+			// Fermeture rs
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+			}catch (Exception ignore) {}
+
+			// Fermeture ps
+			try {
+				if (ps != null)
+					rs.close();
 			}catch (Exception ignore) {}
 
 			// Fermeture con
@@ -193,8 +254,6 @@ public class GestionnaireDAO extends ConnexionBDD{
 					con.close();
 			}catch (Exception ignore) {}
 		}
-		return ajoutEffectue;
+		return listeGroupes;
 	}
 }
-
-
